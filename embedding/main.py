894ce40a2ea96e8f73bd49fcfad63ac2a09a0b87
Hari krishnan <hari.krishnan3@ibm.com>
2023-12-18 15:40:10 +0530

    Added custom retriever

--- embedding/main.py
+++ embedding/main.py
@@ -2,7 +2,6 @@ from langchain.document_loaders import TextLoader
 from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-
 from langchain.vectorstores import Chroma
 
 # 
@@ -10,7 +9,7 @@ load_dotenv()
 
 embeddings = OpenAIEmbeddings()
 
-# eg:
+# eg to calculate embedding manually:
 # emb = embeddings.embed_query("hi there")
 # print(emb)
 
@@ -42,12 +41,29 @@ db = Chroma.from_documents(
 results = db.similarity_search_with_score("What is an interesting fact about english languae ?",
                                           k = 2)
 
-# similarity_search() is another function which returns without score
-
-
-
 for result in results:
     #result is a tuple
     print(result[1])
     print(result[0].page_content)
+
+# similarity_search() is another function which returns without score
+results = db.similarity_search("What is an interesting fact about english languae ?")
+
+for result in results:
+    print(result.page_content)
+    print("\n")
+
+
+
+# similarity_search_by_vector() takes vector as param
+
+# emb = embeddings.embed_query("hi there")
+
+# # find similar documents using the embedding we calculated
+# result = db.max_marginal_relevance_search_by_vector(
+#     embedding = emb,
+#     lambda_mult = 0.8 # range from 0 to 1. Higher values allow similar doc
+# )
+
+
 
--- embedding/prompt.py
+++ embedding/prompt.py
@@ -3,6 +3,11 @@ from langchain.vectorstores import Chroma
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA 
 from langchain.chat_models import ChatOpenAI
+from redundant_fileter_retriever import RedundantFilterRetriever
+import langchain
+
+
+langchain.debug = True
 
 # import langchain
 
@@ -17,16 +22,37 @@ db = Chroma(
     embedding_function = embeddings
 )
 
-retriever = db.as_retriever() # retrive the doc loader function from the database
+# # A retrieval system is defined as something that can take string queries and return the most 'relevant' Documents from some source.
+# retriever = db.as_retriever() # retrive the doc loader function from the database
+
+# chain = RetrievalQA.from_chain_type(
+#     llm = chat,
+#     retriever = retriever, # It is used to work irrespective of the db used, retrivrs the matching vector, 
+#     chain_type = "stuff", # stuffs the matching doc(vector) into the prompt
+#     # verbose=True
+# )
+
+# # other chain_type options are `map_reduce`, `map_rerank`, `refine`
+
+# result = chain.run("What is an interesting fact about english languae ?")
+
+# print(result)
+
+
+
+# using custom retriever
+
+retriever = RedundantFilterRetriever(
+    embeddings = embeddings,
+    chroma = db
+)
 
 chain = RetrievalQA.from_chain_type(
     llm = chat,
-    retriever = retriever, # It is used to work irrespective of the db used
+    retriever = retriever, # It is used to work irrespective of the db used, retrivrs the matching vector, 
     chain_type = "stuff", # stuffs the matching doc(vector) into the prompt
     # verbose=True
 )
-
-# other chain_type options are `map_reduce`, `map_rerank`, `refine`
 
 result = chain.run("What is an interesting fact about english languae ?")
 
